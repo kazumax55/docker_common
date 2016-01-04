a64dcbebf9eec95ab9acd04315b6c05a1dfcf73c
Ken Morishita <k_morishita@yumemi.co.jp>
2016-01-04 15:22:53 +0900

    update rsyslog

--- setup_rsyslog.sh
+++ setup_rsyslog.sh
@@ -13,3 +13,10 @@ EOF
 sed 's/$ModLoad imjournal/# $ModLoad imjournal/' -i /etc/rsyslog.conf
 sed 's/$OmitLocalLogging on/$OmitLocalLogging off/' -i /etc/rsyslog.conf
 sed 's/$IMJournalStateFile imjournal.state/# $IMJournalStateFile imjournal.state/' -i /etc/rsyslog.conf
+sed 's/*.info;mail.none;authpriv.none;cron.none/# *.info;mail.none;authpriv.none;cron.none/' -i /etc/rsyslog.conf
+
+cat <<EOF>>/etc/rsyslog.conf
+*.info;mail.none;authpriv.none;cron.none;local0.none;local1.none        /var/log/messages
+local0.*        /var/log/app/local0
+local1.*        /var/log/app/local1
+EOF
